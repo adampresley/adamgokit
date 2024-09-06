b384d6af6d7786e36fa8329a3f5c7f137e47f81b
Adam Presley <adam@adampresley.com>
2024-09-06 17:04:43 -0500

    Moved interface to new file

--- rendering/GoTemplateRenderer.go
+++ rendering/GoTemplateRenderer.go
@@ -11,18 +11,6 @@ import (
 	"strings"
 )
 
-type TemplateRenderer interface {
-	/*
-	   Render renders a template file into a layout template file using the provided
-	   data to an io.Writer.
-	*/
-	Render(templateName, layoutName string, data any, w io.Writer)
-	/*
-	   RenderString renders a Go template string with a set of data to an io.Writer.
-	*/
-	RenderString(templateString string, data any, w io.Writer)
-}
-
 type GoTemplateRendererConfig struct {
 	AdditionalFuncs   template.FuncMap
 	TemplateDir       string
---
+++ rendering/TemplateRenderer.go
@@ -0,0 +1,15 @@
+package rendering
+
+import "io"
+
+type TemplateRenderer interface {
+	/*
+	   Render renders a template file into a layout template file using the provided
+	   data to an io.Writer.
+	*/
+	Render(templateName, layoutName string, data any, w io.Writer)
+	/*
+	   RenderString renders a Go template string with a set of data to an io.Writer.
+	*/
+	RenderString(templateString string, data any, w io.Writer)
+}
