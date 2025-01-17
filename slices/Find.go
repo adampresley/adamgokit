bef638eed3c67969e3f99b34cad4b584e5c54db6
Adam Presley <adam@adampresley.com>
2025-01-17 17:11:40 -0600

    New method FindWithIndex

--- slices/Find.go
+++ slices/Find.go
@@ -15,3 +15,19 @@ func Find[T any](slice []T, f func(item T) bool) T {
 
 	return result
 }
+
+/*
+FindWithIndex searches for an item in slice []T by calling a predicate function.
+If the function returns true then the item and its index is returned.
+*/
+func FindWithIndex[T any](slice []T, f func(item T) bool) (T, int) {
+	var result T
+
+	for idx, i := range slice {
+		if f(i) {
+			return i, idx
+		}
+	}
+
+	return result, -1
+}
