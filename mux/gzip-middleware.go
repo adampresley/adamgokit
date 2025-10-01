f3e685b311a14c668ac99c4962335bd718918ba3
Adam Presley <adam@adampresley.com>
2025-09-30 20:49:14 -0500

    Refactored gzip middleware to exclude paths

--- mux/gzip-middleware.go
+++ mux/gzip-middleware.go
@@ -7,20 +7,43 @@ import (
 	"strings"
 )
 
-func GzipMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			next.ServeHTTP(w, r)
-			return
-		}
+type GzipMiddlewareConfig struct {
+	excludedPaths []string
+}
 
-		w.Header().Set("Content-Encoding", "gzip")
-		gz := gzip.NewWriter(w)
-		defer gz.Close()
+type GzipMiddlewareConfigOption func(*GzipMiddlewareConfig)
 
-		gzw := &gzipResponseWriter{Writer: gz, ResponseWriter: w}
-		next.ServeHTTP(gzw, r)
-	})
+func NewGzipMiddleware(options ...GzipMiddlewareConfigOption) func(next http.Handler) http.Handler {
+	opts := &GzipMiddlewareConfig{
+		excludedPaths: []string{},
+	}
+
+	for _, opt := range options {
+		opt(opts)
+	}
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			for _, excludedPath := range opts.excludedPaths {
+				if strings.HasPrefix(r.URL.Path, excludedPath) {
+					next.ServeHTTP(w, r)
+					return
+				}
+			}
+
+			w.Header().Set("Content-Encoding", "gzip")
+			gz := gzip.NewWriter(w)
+			defer gz.Close()
+
+			gzw := &gzipResponseWriter{Writer: gz, ResponseWriter: w}
+			next.ServeHTTP(gzw, r)
+		})
+	}
 }
 
 type gzipResponseWriter struct {
@@ -30,4 +53,10 @@ type gzipResponseWriter struct {
 
 func (w *gzipResponseWriter) Write(b []byte) (int, error) {
 	return w.Writer.Write(b)
+}
+
+func WithExcludedPaths(paths ...string) GzipMiddlewareConfigOption {
+	return func(config *GzipMiddlewareConfig) {
+		config.excludedPaths = append(config.excludedPaths, paths...)
+	}
 }
